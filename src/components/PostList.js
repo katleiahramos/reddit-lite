473e45ec64e75e3b1531717ba4074946cc9dcd85
Katleiah Ramos <katleiahramos@gmail.com>
2018-11-16 16:34:58 -0600

    add to do for next step WIP

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,6 +2,7 @@ import React from "react";
 
 const PostList = ({posts}) => {
 
+  // TODO: make <POST> component 
   const renderPosts = () => {
     return posts.map((post, idx) => {
       return <div className="Post" id={idx}>{post.data.title}</div>
