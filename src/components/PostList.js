39a373f41c3a3c9c8652f18e2e2807eef1f20cf2
Katleiah Ramos <katleiahramos@gmail.com>
2018-11-16 11:33:20 -0600

    create PostList component

---
+++ src/components/PostList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostList = props => {
+    return (
+        <div>
+            POSTS GO HERE
+        </div>
+    )
+}
+
+export default PostList
---
+++ src/containers/PostListContainer.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import PostList from "../components/PostList";
+
+class PostListContainer extends Component {
+    state = {
+        posts: []
+    }
+}
