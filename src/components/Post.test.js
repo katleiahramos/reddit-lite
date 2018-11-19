05fdc676b7f8a34c9b01414c831657da26ed62fb
Katleiah Ramos <katleiahramos@gmail.com>
2018-11-19 15:14:25 -0600

    write test for displaying title

--- src/components/Post.test.js
+++ src/components/Post.test.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Post from './Post.js';
-import { configure, shallow} from 'enzyme'
+import { configure, shallow, mount} from 'enzyme'
+import {expect} from 'chai';
 
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
-it('renders without crashing', () => {
-  shallow(<Post />);
-});
+describe('<Post />', () => {
+    it('renders without crashing', () => {
+        const post = {title: "Best Post Ever!", id: 2}
+        shallow(<Post post={post} />);
+      });
+    it('displays post title', () => {
+        const post = {title: "Best Post Ever!", id: 2}
+        const wrapper = shallow(<Post post={post} />)
+        expect(wrapper.text()).to.contain('Best Post Ever!')
+    })
+})
+
