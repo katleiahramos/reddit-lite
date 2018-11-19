35e54e1126da9a72287a73b4efa3752395750103
Katleiah Ramos <katleiahramos@gmail.com>
2018-11-19 14:10:58 -0600

    create test file for App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App.js';
+import { configure, shallow} from 'enzyme'
+
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<App />);
 });
