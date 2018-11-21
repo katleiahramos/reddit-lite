4249302af0730aeb2aa68191687708d2e41f02a6
Katleiah Ramos <katleiahramos@gmail.com>
2018-11-21 09:09:24 -0600

    refactor tests to use post variable

--- src/components/Post.test.js
+++ src/components/Post.test.js
@@ -6,13 +6,17 @@ import {expect} from 'chai';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
+
+
+
 describe('<Post />', () => {
+    let post = {title: "Best Post Ever!", id: 2}
     it('renders without crashing', () => {
-        const post = {title: "Best Post Ever!", id: 2}
+        // const post = {title: "Best Post Ever!", id: 2}
         shallow(<Post post={post} />);
       });
     it('displays post title', () => {
-        const post = {title: "Best Post Ever!", id: 2}
+        // const post = {title: "Best Post Ever!", id: 2}
         const wrapper = shallow(<Post post={post} />)
         expect(wrapper.text()).to.contain('Best Post Ever!')
     })
