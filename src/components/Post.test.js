1ad92440dafff137b8ad40b8581a4478a1e0dbc3
Katleiah Ramos <katleiahramos@gmail.com>
2018-11-21 09:09:55 -0600

    delete unsed code from Post tests

--- src/components/Post.test.js
+++ src/components/Post.test.js
@@ -12,11 +12,9 @@ configure({ adapter: new Adapter() });
 describe('<Post />', () => {
     let post = {title: "Best Post Ever!", id: 2}
     it('renders without crashing', () => {
-        // const post = {title: "Best Post Ever!", id: 2}
         shallow(<Post post={post} />);
       });
     it('displays post title', () => {
-        // const post = {title: "Best Post Ever!", id: 2}
         const wrapper = shallow(<Post post={post} />)
         expect(wrapper.text()).to.contain('Best Post Ever!')
     })
