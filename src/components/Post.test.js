9c95edaba0dd662bc6dcf634796bcbf1420a42b9
Katleiah Ramos <katleiahramos@gmail.com>
2018-11-21 10:07:48 -0600

    add test for link to comments

--- src/components/Post.test.js
+++ src/components/Post.test.js
@@ -10,17 +10,21 @@ configure({ adapter: new Adapter() });
 
 
 describe('<Post />', () => {
-    const post = {title: "Best Post Ever!", id: 2, url: "www.post.com"}
+    const post = {title: "Best Post Ever!", id: 2, url: "www.post.com", permalink: "/r/posts/thispost"}
     const postWrapper = shallow(<Post post={post} />)
     it('renders without crashing', () => {
         postWrapper;
       });
     it('displays post title', () => {
         // const postWrapper = shallow(<Post post={post} />)
-        expect(postWrapper.text()).to.contain('Best Post Ever!')
+        expect(postWrapper.text()).to.contain(post.title)
     })
     it('displays post title linking to the shared post or site', () => {
-        expect(postWrapper.html()).to.contain('<a href="www.post.com">')
+        expect(postWrapper.html()).to.contain(`<a href="${post.url}">`)
     })
+    it('displays link to the comments on the reddit site ', () => {
+        expect(postWrapper.html()).to.contain(`<a href="www.reddit.com${post.permalink}">`)
+    })
+    
 })
 
